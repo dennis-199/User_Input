e8a9e86aa10b181abef159c76585232ed51df084
dennis-199 <otieno.dennis@strathmore.edu>
2021-12-02 17:57:08 +0300

    date picker

--- app/src/main/java/com/example/userinput/MainActivity.java
+++ app/src/main/java/com/example/userinput/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.userinput;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText date;
+    ImageView cal;
+    private int mDate, mMonth, mYear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        date = findViewById(R.id.date);
+        cal = findViewById(R.id.datepicker);
+
+        cal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar Cal = Calendar.getInstance();
+                mDate = Cal.get(Calendar.DATE);
+                mMonth = Cal.get(Calendar.MONTH);
+                mYear = Cal.get(Calendar.YEAR);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        date.setText(dayOfMonth+"/"+month+"/"+year);
+                    }
+                }, mYear, mMonth, mDate);
+                datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis()-1000);
+                datePickerDialog.show();
+            }
+        });
     }
 }
